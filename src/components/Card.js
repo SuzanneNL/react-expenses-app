e01a70a4cefd02117eb123ce39711b3c6b7cbcf4
suzannenl <skr470@gmail.com>
2021-12-03 20:46:22 +0100

    Create Card components, practice composition (children props)

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import "./Card.css";
+
+const Card = (props) => {
+  //This adds the classNames from the className prop:
+  const classes = "card " + props.className;
+
+  // props.children is used to add the data between the opening and closing JSX tags:
+  return <div className={classes}>{props.children}</div>;
+};
+
+export default Card;
