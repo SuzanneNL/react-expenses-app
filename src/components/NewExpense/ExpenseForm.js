2e1423f7481987aeb93acb374fa3a68c989b749d
suzannenl <skr470@gmail.com>
2021-12-03 22:05:42 +0100

    Get title value that is entered by a user

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,24 +3,30 @@ import React from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  // This gets the default event object as an object:
+  const titleChangeHandler = (event) => {
+    // We can then access the value that was entered:
+    console.log(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01"/>
+          <input type="number" min="0.01" step="0.01" />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31"/>
+          <input type="date" min="2019-01-01" max="2022-12-31" />
         </div>
       </div>
       <div className="new-expense__actions">
-          <button type="submit">Add Expense</button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
