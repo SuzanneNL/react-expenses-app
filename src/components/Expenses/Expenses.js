efe17984110e72ef41a8013d2c4ee902e3a73070
suzannenl <skr470@gmail.com>
2021-12-10 21:00:46 +0100

    Complete expenses chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -17,14 +18,14 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <ExpensesList items={filteredExpenses}/>
+      <ExpensesChart expenses={filteredExpenses}/>
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
